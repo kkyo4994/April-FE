f2f5cad7fcf5e14b117e562ff2358c5e01b35f8f
kky04994 <kkyo8747@naver.com>
2022-05-17 23:34:00 +0900

    🎨 (#3) - add route folder

---
+++ april/src/route/MainRouter.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+const MainRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<div>요!</div>} />
+      </Routes>
+    </>
+  );
+};
+
+export default MainRouter;
