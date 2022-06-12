a5f6b533615b588663c211d3fb953fdfaf4b2d54
kky04994 <kkyo8747@naver.com>
2022-06-13 02:12:19 +0900

    🔮 (#15) - add writemutation code

--- april/src/components/Write/index.tsx
+++ april/src/components/Write/index.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import { useWriteLetter } from "../../queries/Letter";
 import * as S from "./style"
 
 const Write = () => {
+  const writeMutation = useWriteLetter();
+  const [letterTitle, setLetterTitle] = useState("");
+  const [letterFeeling, setLetterFeeling] = useState("");
+  const [letterContent, setLetterContent] = useState("");
+  const [letterSong, setLetterSong] = useState("");
+
+  const onClickWriteBtn = () => {
+    writeMutation.mutate(
+      {
+        title:letterTitle,
+        feeling: letterFeeling,
+        song: letterSong,
+        content: letterContent,
+      }
+    )
+  }
+
     return (
       <S.Container>
-          <S.Input type="text" placeholder="제목을 작성해 주세요." />
-          <S.Input type="text" placeholder="오늘의 기분을 작성해 주세요." />
-          <S.Input type="text" placeholder="오늘의 노래를 작성해 주세요." />
-         <S.TextareaBox placeholder="오늘 있었던 일과 한 달 후의 자신에게 보낼 말을 작성해 주세요." />
-        <S.WriteBtn disabled={false}>작성하러 가기</S.WriteBtn>
+          <S.Input type="text" placeholder="제목을 작성해 주세요."  onChange={e => setLetterTitle(e.target.value)}/>
+          <S.Input type="text" placeholder="오늘의 기분을 작성해 주세요." onChange={e => setLetterFeeling(e.target.value)}/>
+          <S.Input type="text" placeholder="오늘의 노래를 작성해 주세요." onChange={e => setLetterSong(e.target.value)} />
+         <S.TextareaBox placeholder="오늘 있었던 일과 한 달 후의 자신에게 보낼 말을 작성해 주세요." onChange={e => setLetterContent(e.target.value)} />
+        <S.WriteBtn onClick={onClickWriteBtn} disabled={letterContent=== "" || letterFeeling === "" || letterSong === "" || letterTitle === ""}>작성하러 가기</S.WriteBtn>
       </S.Container>
     )   
 }
