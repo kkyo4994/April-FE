a052e5a2b2ccde658e8057b6abdeea0020831279
kky04994 <kkyo8747@naver.com>
2022-06-12 22:29:10 +0900

    🔮 (#10) - add axios file

---
+++ april/src/utils/axios/index.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosError } from "axios";
+import baseURL from "./BaseUrl";
+import { refresh } from "./refresh";
+import toast from "react-hot-toast";
+
+const request = axios.create({
+  baseURL: baseURL,
+  timeout: 100000,
+});
+
+export const instance = axios.create({
+  baseURL: baseURL,
+  timeout: 100000,
+});
+
+request.interceptors.request.use(refresh, function (error: AxiosError) {
+  toast.error("네트워크를 확인해주세요.");
+  return Promise.reject(error);
+});
+
+request.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  }
+);
+
+export default request;
