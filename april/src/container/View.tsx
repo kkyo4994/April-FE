c92683ec8bfec71230970a5b3d6329e9e1225ce3
kky04994 <kkyo8747@naver.com>
2022-06-12 15:54:07 +0900

    🔮 (#9) - add ViewContainer file

---
+++ april/src/container/View.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import View from "../components/View";
+
+const ViewContainer = () => {
+    return (
+        <>
+        <Header />
+        <View />
+        </>
+    )
+}
+
+export default ViewContainer;
