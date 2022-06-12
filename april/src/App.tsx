1936652f342cb4efeff8cbadade149dfb3e2cef8
kky04994 <kkyo8747@naver.com>
2022-06-13 00:56:51 +0900

    🔮 (#13) - add QueryClientProvider code

--- april/src/App.tsx
+++ april/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import RootRouter from "./route";
 import GlobalStyle from "./style/GlobalStyle";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <RootRouter />
+      </QueryClientProvider>
     </>
   );
 }
